83721ab51ada83863bc433d394095852dc12fe52
Aditya Singh <s.aditya.digi@gmail.com>
2025-03-31 01:06:07 +0530

    Update CV, added new projects

--- lib/data.ts
+++ lib/data.ts
@@ -7,6 +7,8 @@ import rmtdevImg from "@/public/rmtdev.png";
 import wordanalyticsImg from "@/public/wordanalytics.png";
 import pubmed from "@/public/pubMed.png"
 import trans from "@/public/trans.png"
+import bwise from "@/public/bwise.png"
+import ghubProfile from "@/public/ghubProfile.png"
 
 export const links = [
   {
@@ -37,13 +39,13 @@ export const links = [
 
 export const experiencesData = [
   {
-    title: "Data Scientist Intern at United Mentors Pvt. Ltd",
+    title: "AI Trainer at Outlier AI",
     location: "Remote, India",
     description:
-      "Developed Power BI dashboards for Bird Strikes data, enhancing insights on aircraft impacts, financial loss, and fatalities.",
-    icon: React.createElement(LuGraduationCap),
-    date: "May 2024 - Jul 2024",
-  },
+      "Working as a freelance AI developer, building intelligent solutions across various domains.",
+    icon: React.createElement(FaReact),
+    date: "Jan 2025 - present",
+  }, 
   {
     title: "Computer Vision Intern at Oriental InfraTrust Pvt. Ltd",
     location: "Kanpur, India",
@@ -53,28 +55,45 @@ export const experiencesData = [
     date: "Jul 2024 - Sep 2024",
   },
   {
-    title: "Freelance AI Developer",
+    title: "Data Scientist Intern at United Mentors Pvt. Ltd",
     location: "Remote, India",
     description:
-      "Working as a freelance AI developer, building intelligent solutions across various domains.",
-    icon: React.createElement(FaReact),
-    date: "Jan 2025 - present",
+      "Developed Power BI dashboards for Bird Strikes data, enhancing insights on aircraft impacts, financial loss, and fatalities.",
+    icon: React.createElement(LuGraduationCap),
+    date: "May 2024 - Jul 2024",
   },
+ 
 ] as const;
 
 export const projectsData = [
   {
+    title: "Budget Wise",
+    description:
+      "A next generation budget tracking app to track all your spendings in a streamlined manner.",
+    tags: ["NextJS", "TypeScript", "Prisma", "Neon", "PostgreSQL"],
+    imageUrl: bwise,
+    link: "https://bwise.vercel.app/",
+  },
+  {
     title: "Speech Transcription and Translation",
     description:
-      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
+      "App to transcribe and translate audio using Whisper from OpenAI.",
     tags: ["DeepLearning", "WhisperAI", "OpenAI", "Gradio", "HuggingFace"],
     imageUrl: trans,
     link: "https://huggingface.co/spaces/diaL42/speechTranscription",
   },
   {
+    title: "Github Profile Finder",
+    description:
+      "Helps you find any profile on Github and see it in detail without opening Github.",
+    tags: ["NextJS", "RestAPI", "TailWind CSS"],
+    imageUrl: ghubProfile,
+    link: "https://profilelookupgit.vercel.app/",
+  },
+  {
     title: "PubMed Scrapper and Combined Summarizer",
     description:
-      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
+      "Downloads and summarizes medical research papers from PubMed based on user keyword",
     tags: ["BioPython", "WebScrapper", "Bart", "HuggingFace"],
     imageUrl: pubmed,
     link: "https://github.com/TheGuyCheese/PubMedScrapperandCombinedSummarizer",
